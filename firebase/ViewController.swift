065d11e80dd011b3133d7cb18f32992b0db78e92
ashuforweb <ashuforweb@gmail.com>
2016-01-30 03:39:04 -0800

    0.02a

--- firebase/ViewController.swift
+++ firebase/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("I can't wait to push this project to Github")
+        
     }
 
     override func didReceiveMemoryWarning() {
